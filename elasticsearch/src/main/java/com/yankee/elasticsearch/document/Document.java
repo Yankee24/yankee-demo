96900ced53d8c004b594956b7e4d0b98144f3f38
Yankee <yangqi199808@gmail.com>
2021-09-14 10:27:46 +0800

    :sparkles: Document父类

---
+++ elasticsearch/src/main/java/com/yankee/elasticsearch/document/Document.java
@@ -0,0 +1,14 @@
+package com.yankee.elasticsearch.document;
+
+import lombok.Data;
+
+/**
+ * @author Yankee
+ * @program yankee-demo
+ * @description
+ * @since 2021/9/13
+ */
+@Data
+public abstract class Document {
+    String id = "";
+}
