b3b8d28b3f2b8a41e98e141505a0e12d3a0e3841
Yankee <yangqi199808@gmail.com>
2021-09-14 16:14:03 +0800

    :sparkles: 增加search接口(searchAllDocument)

--- elasticsearch/src/main/java/com/yankee/elasticsearch/controller/ElasticSearchSearchController.java
+++ elasticsearch/src/main/java/com/yankee/elasticsearch/controller/ElasticSearchSearchController.java
@@ -1,11 +1,16 @@
 package com.yankee.elasticsearch.controller;
 
+import com.yankee.elasticsearch.document.UserDocument;
 import com.yankee.elasticsearch.service.ElasticSearchSearchService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @author Yankee
@@ -18,5 +23,11 @@ import javax.annotation.Resource;
 @Slf4j
 public class ElasticSearchSearchController {
     @Resource
-    private ElasticSearchSearchService searchService;
+    private ElasticSearchSearchService<UserDocument> searchService;
+
+    @GetMapping("all")
+    public List<UserDocument> searchAllDocument(@RequestParam(value = "index") String index) throws IOException {
+        log.info("索引为：{}", index);
+        return searchService.searchAllDocument(index, UserDocument.class);
+    }
 }
