4e8d90792b37332cbc3f6b7953a0881d18af4e9c
Yankee <yangqi199808@gmail.com>
2021-09-14 10:29:47 +0800

    :sparkles: 增加document对外接口

---
+++ elasticsearch/src/main/java/com/yankee/elasticsearch/controller/ElasticSearchDocumentController.java
@@ -0,0 +1,22 @@
+package com.yankee.elasticsearch.controller;
+
+import com.yankee.elasticsearch.service.ElasticSearchDocumentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Yankee
+ * @program yankee-demo
+ * @description
+ * @since 2021/9/13
+ */
+@RestController
+@RequestMapping("/elasticsearch/document")
+@Slf4j
+public class ElasticSearchDocumentController {
+    @Resource
+    private ElasticSearchDocumentService documentService;
+}
